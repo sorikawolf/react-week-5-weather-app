6712174a4ec382afb2daacb0630e13689945e7ff
sorikawolf <jennsorikawolf@gmail.com>
2026-07-05 21:06:12 -0400

    Added link to Github repository

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ function App() {
   return (
     <div className="App">
       <h1>React Week 5 Weather App</h1>
+      <footer>
+        This project was coded by Sorika Wolf and is{" "}
+        <a
+          href="https://github.com/sorikawolf/react-week-5-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }

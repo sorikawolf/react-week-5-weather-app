fd48adddbe77fad5c4e775c4561e6986ca825d32
sorikawolf <jennsorikawolf@gmail.com>
2026-07-06 20:31:27 -0400

    added basic styling to Weather component

--- src/Weather.js
+++ src/Weather.js
@@ -5,21 +5,37 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <input type="search" placeholder="Enter a city" />
-        <input type="submit" value="Search" className="btn btn-primary" />
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
       </form>
       <h1>New York</h1>
       <ul>
         <li>Monday 5:08pm</li>
         <li>Light Rain</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
           <img
             src="https://www.accuweather.com/images/weathericons/v2a/07.svg"
             alt="cloudy"
           />
-          65°C
+          <span className="temperature">65</span>{" "}
+          <span className="unit">°C</span>
         </div>
         <div className="col-6">
           <ul>

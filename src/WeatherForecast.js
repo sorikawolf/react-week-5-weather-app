54fb6f8f1dd143fe53b931bfe9956c3b4e35e10c
sorikawolf <jennsorikawolf@gmail.com>
2026-07-27 19:53:47 -0400

    added max and min temperatures

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
+  const [forecast, setForecast] = useState("null");
+
   function handleResponse(response) {
     console.log(response);
+    console.log(response.data.daily[0].temperature.maximum);
+    setForecast({
+      maxTemp: response.data.daily[0].temperature.maximum,
+      minTemp: response.data.daily[0].temperature.minimum,
+    });
   }
 
-  const city = props.city;
+  let city = props.city;
   let apiKey = "357affaact442eeoc3f4199173062fb9";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
 
@@ -21,8 +28,8 @@ export default function WeatherForecast(props) {
           <div>Thurs</div>
           <div className="ForecastIcon">icon</div>
           <div>
-            <span className="ForecastMax">19°</span>
-            <span className="ForecastMin">10°</span>
+            <span className="ForecastMax">{Math.round(forecast.maxTemp)}°</span>
+            <span className="ForecastMin">{Math.round(forecast.minTemp)}°</span>
           </div>
         </div>
       </div>

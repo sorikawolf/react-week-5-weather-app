a8c56424d67781a6193098ca92120a94cebaa8d5
sorikawolf <jennsorikawolf@gmail.com>
2026-07-27 23:25:18 -0400

    added forecast info from api call
    must redo styling

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,35 +4,49 @@ import axios from "axios";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
-  const [forecast, setForecast] = useState("null");
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
   function handleResponse(response) {
     console.log(response);
     console.log(response.data.daily[0].temperature.maximum);
+    setLoaded(true);
     setForecast({
       maxTemp: response.data.daily[0].temperature.maximum,
       minTemp: response.data.daily[0].temperature.minimum,
+      iconUrl: response.data.daily[0].condition.icon_url,
+      condition: response.data.daily[0].condition.desription,
     });
   }
 
-  let city = props.city;
-  let apiKey = "357affaact442eeoc3f4199173062fb9";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div>Thurs</div>
-          <div className="ForecastIcon">icon</div>
-          <div>
-            <span className="ForecastMax">{Math.round(forecast.maxTemp)}°</span>
-            <span className="ForecastMin">{Math.round(forecast.minTemp)}°</span>
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <div>Thurs</div>
+            <div className="ForecastIcon">
+              <img src={forecast.iconUrl} alt={forecast.condition} />
+            </div>
+            <div>
+              <span className="ForecastMax">
+                {Math.round(forecast.maxTemp)}°
+              </span>
+              <span className="ForecastMin">
+                {Math.round(forecast.minTemp)}°
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let city = props.city;
+    let apiKey = "357affaact442eeoc3f4199173062fb9";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
 }

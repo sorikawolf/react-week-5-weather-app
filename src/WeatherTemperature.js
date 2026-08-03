69d6ab7db159f91b11436db46cf946e8d1cb71f8
sorikawolf <jennsorikawolf@gmail.com>
2026-08-03 12:46:42 -0400

    added useEffect to update forecast with every search

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,42 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function displayFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function displayCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celsius)}</span>{" "}
-        <span className="unit">
-          °C |{" "}
-          <a href="/" onClick={displayFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(fahrenheit)}</span>{" "}
-        <span className="unit">
-          {" "}
-          <a href="/" onClick={displayCelsius}>
-            °C{" "}
-          </a>
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celsius)}</span>{" "}
+      <span className="unit">°C</span>
+    </div>
+  );
 }
